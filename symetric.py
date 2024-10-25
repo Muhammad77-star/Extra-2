2a23a6956290fc2fcdd7f156e4e4f3617ec22b54
Muhammad77-star <hussainizyaan@gmail.com>
2024-10-25 16:56:00 +0400

    fff

---
+++ symetric.py
@@ -0,0 +1,8 @@
+set1_a = {'blue', 'green'}
+set2_a = {'blue', 'yellow'}
+symmetric_diff_a = set1_a.symmetric_difference(set2_a)
+print(f"Symmetric difference for A: {symmetric_diff_a}")
+set1_b = {1, 2, 3, 4, 5}
+set2_b = {1, 5, 6, 7, 8, 9}
+symmetric_diff_b = set1_b.symmetric_difference(set2_b)
+print(f"Symmetric difference for B: {symmetric_diff_b}")
